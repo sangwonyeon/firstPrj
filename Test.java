15b0c6f96790fe8b96fe2bb91a8f40201c099eb4
sangwonyeon <hiyoun@gmail.com>
2020-11-11 01:19:48 +0900

    * merge test 2

--- Test.java
+++ Test.java
@@ -8,9 +8,9 @@ public class Test {
 		System.out.println("33333");
 		
 		
-		System.out.println("55555");
+		System.out.println("55555");//local
 		
-		System.out.println("4444");
+		System.out.println("4444");//remote
 		
 	}
 
